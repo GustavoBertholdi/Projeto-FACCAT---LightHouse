bd025b05ae1e2c6a993b228254ea3d1aec9a8d81
Gustavo Bertholdi <101758282+GustavoBertholdi@users.noreply.github.com>
2022-05-23 20:04:08 -0300

    Versão 1.0
    Ainda não é o projeto final por conta que sou meio leigo ainda com javascript.

--- _javascript/script.js
+++ _javascript/script.js
@@ -1,5 +1,5 @@
 let dino = document.querySelector('#dino');
-let cacto = document.querySelector('#quadrado');
+let cacto = document.querySelector('#cacto');
 var jump = false;
 function jump1(){
     if(dino.classList != 'jump') {
@@ -11,15 +11,3 @@ function jump1(){
         jump = false;
     }, 500)
 }
-    
-function dead(){
-    var leftCacto = parseInt(window.getComputedStyle(cacto).getPropertyValue('left'))
-
-    if(leftCacto > -100 && jump == false){
-        cacto.style.animation = 'none'
-        cacto.style.display = 'none'
-        document.write("Você perdeu! Pressione f5 para voltar!")
-    }
-}
-
-
